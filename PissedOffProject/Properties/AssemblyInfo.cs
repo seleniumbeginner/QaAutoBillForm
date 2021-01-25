6e1e30967f936dbb22c1ea6f292e49d0e9c2a3d7
basanta godar <mailgodar@gmail.com>
2021-01-25 13:23:44 +1300

    Add project files.

---
+++ PissedOffProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PissedOffProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PissedOffProject")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("31100e13-fe5f-4b55-9f21-af82c7be6c92")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PissedOffProject/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace PissedOffProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        IWebDriver driver = new ChromeDriver();
+        [TestMethod]
+        public void TestMethod()
+        {
+            
+            driver.Navigate().GoToUrl("http://qaauto.co.nz/test-blank-form/");
+            driver.FindElement(By.Id("wpforms-locked-16-field_form_locker_password")).SendKeys("Testing");
+            driver.FindElement(By.Name("wpforms[submit]")).Click();
+            driver.Manage().Window.Maximize();
+           FillForm();
+
+
+        }
+        public void FillForm()
+        {
+            driver.FindElement(By.Id("wpforms-16-field_0")).SendKeys("Ajit");
+            driver.FindElement(By.Id("wpforms-16-field_0-last")).SendKeys("Sharma");
+            driver.FindElement(By.Id ("wpforms-16-field_1")).SendKeys("ajit@gmail.com");
+            driver.FindElement(By.Id("wpforms-16-field_2")).SendKeys("Enter your comment Here");
+            
+            driver.FindElement(By.TagName ("button")).Click ();
+            driver.FindElement(By.TagName   ("p")).SendKeys("finally its over");
+        //  Console.WriteLine("Finally completed without POM model ");
+
+
+    }
+    }
+}
